f6cf127c2c448602582cfeed12b3d9378fdf5d7b
amritesh-18 <amriteshx18@gmail.com>
2024-09-16 11:23:53 +0530

    Completed Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    
+    if(num1<0||num2 <0||Number.isInteger(num1)!=true||Number.isInteger(num2)!=true){
+        return "ERROR";
+    }
+    let result=0;
+    if(num1<num2){
+        for(let i=num1;i<=num2;i++){
+        result+=i;}
+    } else if (num1>num2) {
+        for(let i=num1;i>=num2;i--){
+        result+=i;}
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
