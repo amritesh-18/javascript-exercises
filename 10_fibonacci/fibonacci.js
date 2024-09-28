0e45446ee8df260d876f3226dedf042ac085a105
amritesh-18 <amriteshx18@gmail.com>
2024-09-28 15:43:17 +0530

    Completed Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let count
+    if (typeof n !== 'number') {
+        count = parseInt(n)
+    } else {
+        count = n;
+    }
 
+    if(n<0) return "OOPS";
+    if(count == 0) return 0;
+
+    let prev = 1;
+    let next = 0;
+
+    for(let i = 2; i<=n; i++){
+        let current = prev + next;
+        next = prev;
+        prev = current;
+    }
+
+    return prev;
 };
 
 // Do not edit below this line
