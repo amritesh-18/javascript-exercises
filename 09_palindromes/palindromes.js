012a84247c89f25a87797d4187488e60dbb46b29
amritesh-18 <amriteshx18@gmail.com>
2024-09-28 15:11:58 +0530

    Completed Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const condition = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const newStr = str.toLowerCase()
+                  .split('')
+                  .filter((char) => condition.includes(char))
+                  .join('');
 
+    return newStr === newStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
