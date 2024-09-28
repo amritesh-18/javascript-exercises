3aaad919620466c202294802071075a73414fd4e
amritesh-18 <amriteshx18@gmail.com>
2024-09-28 14:39:39 +0530

    Completed exercise eight

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length == 0){
+    return 0;
+  }
+	let total = 0;
+  for(let i = 0; i<arr.length; i++){
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total =1;
+  for(let i = 0;i<arr.length;i++){
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n==0) return 1;
+  result = 1;
+  for(let i = 1; i<=n; i++){
+    result*= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
