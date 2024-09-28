0a11b05f0a241c0c05d5dd663fc07c2e8c8a0b34
amritesh-18 <amriteshx18@gmail.com>
2024-09-28 16:21:24 +0530

    Completed Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldest = null;
 
+    for (const person of arr) {
+        const yearOfDeath = person.yearOfDeath || new Date().getFullYear();
+        const age = yearOfDeath - person.yearOfBirth;
+
+        if (!oldest || age > (oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth) {
+            oldest = person;
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
